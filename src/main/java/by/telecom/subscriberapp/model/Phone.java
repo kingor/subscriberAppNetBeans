0cfc60380e7d2799b486c9a8aaa9def3ac331638
kingor <kingor333@mail.ru>
2015-03-10 15:39:17 -0300

    edit Subscriber

---
+++ src/main/java/by/telecom/subscriberapp/model/Phone.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package by.telecom.subscriberapp.model;
+
+/**
+ *
+ * @author Family_P
+ */
+public class Phone {
+    private Long id;
+    private Long id_subscriber;
+}
